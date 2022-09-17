a6d39d4efce4b7e49ec020b2715e230f6c859404
Noel <112729963+NoelH98@users.noreply.github.com>
2022-09-17 20:52:36 +0300

    Add files via upload

---
+++ node.js_blog_express_mongodb/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1/test',
+   {useNewUrlParser:true,  
+   useUnifiedTopology:true,
+   usecreateIndex: true,
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', async (req, res) => {
+   const articles = await Article.find().sort({
+      createdAt: 'desc'
+   });
+    res.render('index',{articles:articles});
+});
+app.use('/articles',articleRouter);
+
+app.listen(5000);
